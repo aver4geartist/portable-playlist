c9d4c6ca0f473c96d12ef4c45f2de5052849181a
aver4geartist <born2rush2max@gmail.com>
2025-03-09 22:16:47 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+// list of songs in the repository
+const songs = [
+    { name: "Resonance - Perfect Slowed", artist: "Noir/DRXYL", file: "songs/Resonance - Perfect Slowed.mp3" }
+];
+
+let playlist = document.getElementById("playlist");
+let audioPlayer = document.getElementById("audio-player");
+
+songs.forEach(song => {
+    let songItem = document.createElement("li");
+    songItem.innerHTML = `
+        <div class="song-info">
+            <img src="https://cdn-icons-png.flaticon.com/512/26/26630.png" alt="music icon">
+            <div>
+                <strong>${song.name}</strong><br>
+                <small>${song.artist}</small>
+            </div>
+        </div>
+    `;
+
+    songItem.addEventListener("click", () => {
+        audioPlayer.src = song.file;
+        audioPlayer.play();
+    });
+
+    playlist.appendChild(songItem);
+});
